e6d9541df19154e72c58b85e79b829a80c1c05a0
Nikki <bravonikki13@gmail.com>
2025-04-22 14:56:38 +0200

    Updated command deployment script to match the other loaders

--- src/utility/register.ts
+++ src/utility/register.ts
@@ -1,53 +1,70 @@
-import { REST, Routes } from 'discord.js';
+import {
+  REST,
+  Routes,
+  type APIApplicationCommandSubcommandGroupOption,
+  type APIApplicationCommandSubcommandOption,
+  type RESTPostAPIChatInputApplicationCommandsJSONBody,
+} from 'discord.js';
 import { performance } from 'perf_hooks';
 
 import type { Command } from 'classes/base/command';
 
 import { getCommandFiles } from 'utility/files';
 import { KEYS } from 'utility/keys';
-import logger from 'utility/logger';
+import logger, { table } from 'utility/logger';
 import { initializeI18N, translateCommand } from 'utility/translation';
 
 await initializeI18N('commands');
 
-logger.info('Loading command files');
+const commands: (
+  | RESTPostAPIChatInputApplicationCommandsJSONBody
+  | APIApplicationCommandSubcommandOption
+  | APIApplicationCommandSubcommandGroupOption
+)[] = [];
+const tableData: { file: string; name: string; valid: string }[] = [];
 const startTime = performance.now();
-
-const commands = [];
 const filePaths = await getCommandFiles();
 
-for (const filePath of filePaths) {
-  const command = (await import(filePath)).default as Command;
+await Promise.all(
+  filePaths.map(async (filePath) => {
+    const command = (await import(`${filePath}?update=${Date.now()}`)).default;
 
-  if ('builder' in command.options && 'execute' in command.options) {
-    commands.push(command.options.builder.toJSON());
-    logger.info(`Loaded command file ${filePath.split('/').slice(-2).join('/')} (${command.options.builder.name})`);
-  } else {
-    logger.warn(`Command file ${filePath} is missing data or execute`);
-  }
-}
+    if (isValidCommand(command)) {
+      commands.push(command.options.builder.toJSON());
+      tableData.push({
+        file: filePath.split('/').slice(-2).join('/'),
+        name: command.options.builder.name,
+        valid: '✅',
+      });
+      logger.debug(`Loaded command file ${filePath.split('/').slice(-2).join('/')} (${command.options.builder.name})`);
+    } else {
+      tableData.push({
+        file: filePath.split('/').slice(-2).join('/'),
+        name: command?.options?.builder?.name || 'undefined',
+        valid: '❌',
+      });
+      logger.warn(`Command file ${filePath} is missing data or execute`);
+    }
+  }),
+);
 
 const endTime = performance.now();
 logger.info(
-  `Loaded ${filePaths.length} command${filePaths.length > 1 || filePaths.length === 0 ? 's' : ''} in ${Math.floor(endTime - startTime)}ms`,
+  `Loaded ${filePaths.length} command${filePaths.length > 1 || filePaths.length === 0 ? 's' : ''} in ${Math.floor(endTime - startTime)}ms\n${table(tableData)}`,
 );
 
 const rest = new REST().setToken(KEYS.DISCORD_BOT_TOKEN);
 
 (async () => {
   try {
-    logger.info(`Started reloading ${commands.length} application (/) command${commands.length > 1 || commands.length === 0 ? 's' : ''}.`);
+    logger.info(`Starting to reload application (/) commands.`);
 
     const deployStartTime = performance.now();
-
     const commandsWithTranslation = commands.map(translateCommand);
 
-    const data = (await rest.put(Routes.applicationCommands(KEYS.DISCORD_BOT_ID), {
+    await rest.put(Routes.applicationCommands(KEYS.DISCORD_BOT_ID), {
       body: commandsWithTranslation,
-    })) as unknown as {
-      id: string;
-      name: string;
-    }[];
+    });
 
     if (KEYS.DISCORD_DEV_GUILD_ID) {
       await rest.put(Routes.applicationGuildCommands(KEYS.DISCORD_BOT_ID, KEYS.DISCORD_DEV_GUILD_ID), {
@@ -57,10 +74,19 @@ const rest = new REST().setToken(KEYS.DISCORD_BOT_TOKEN);
 
     const deployEndTime = performance.now();
 
-    logger.info(
-      `Successfully reloaded ${data.length} application (/) command${commands.length > 1 || commands.length === 0 ? 's' : ''} in ${Math.floor(deployEndTime - deployStartTime)}ms`,
-    );
+    logger.info(`Successfully reloaded application (/) commands in ${Math.floor(deployEndTime - deployStartTime)}ms!`);
   } catch (error) {
-    logger.error({ err: error }, 'Failed to refresh application (/) commands');
+    logger.error({ err: error }, 'Failed to refresh application (/) commands.');
   }
 })();
+
+function isValidCommand(command: Command): command is Command {
+  return (
+    typeof command === 'object' &&
+    command !== null &&
+    typeof command.options === 'object' &&
+    command.options !== null &&
+    'builder' in command.options &&
+    'execute' in command.options
+  );
+}
